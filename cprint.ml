702e37616c252fbb72e7ce30d25e03ee94ac7525
Vanille-N <neven.villani@gmail.com>
2020-11-19 11:05:21 +0100

    maybe this will be of use

--- cprint.ml
+++ cprint.ml
@@ -24,7 +24,28 @@ let cmp_op_repr = function
     | C_LE -> "LE"
     | C_EQ -> "EQ"
 
-let print_ast out dec_list =
+let rec inline_code code =
+    match snd code with
+        | CBLOCK _ -> false
+        | CEXPR expr -> inline_expr expr
+        | CIF _ -> false
+        | CWHILE _ -> false
+        | CRETURN None -> true
+        | CRETURN (Some ret) -> inline_expr ret
+and inline_expr expr =
+    match snd expr with
+        | VAR _ -> true
+        | CST _ -> true
+        | STRING _ -> true
+        | SET_VAR (_, expr) -> inline_expr expr
+        | SET_ARRAY (_, index, value) -> (inline_expr index) && (inline_expr value)
+        | CALL _ -> false
+        | OP1 (_, expr) -> inline_expr expr
+        | OP2 (_, lhs, rhs) -> (inline_expr lhs) && (inline_expr rhs)
+        | CMP (_, lhs, rhs) -> (inline_expr lhs) && (inline_expr rhs)
+        | EIF _ -> false
+        | ESEQ _ -> false
+
     List.iter (function
         | CDECL(_, name) -> fprintf out "decl name <%s>\n" name
         | CFUN(_, name, decs, code) -> (
