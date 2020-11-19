2879f2b4d284810a6e06e7c429027140966bd3ac
Vanille-N <neven.villani@gmail.com>
2020-11-19 11:04:48 +0100

    more colors

--- pigment.ml
+++ pigment.ml
@@ -1,7 +1,10 @@
 open Printf
 
 let color c s =
-    sprintf "\x1b[%sm%s\x1b[0m" c s
+    sprintf "\x1b[1;%sm%s\x1b[0m" c s
 
 let red = color "31"
 let green = color "32"
+let yellow = color "33"
+let blue = color "34"
+let purple = color "35"
--- pigment.mli
+++ pigment.mli
@@ -1,2 +1,5 @@
 val red : string -> string
 val green : string -> string
+val yellow : string -> string
+val blue : string -> string
+val purple : string -> string
