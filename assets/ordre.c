8c884c734346d25ff1b94d2fc25c1615239ecdb7
Vanille-N <neven.villani@gmail.com>
2020-11-19 07:05:25 +0100

    c tests

---
+++ assets/ordre.c
@@ -0,0 +1,13 @@
+#ifndef MCC
+#include<stdio.h>
+#endif
+
+int main (int argc, char **argv)
+{
+  int i, j;
+
+  j = (i=3) + (i=4);
+  printf ("Valeur de j=%d (normalement 7), valeur de i=%d.\n", j, i);
+  fflush (stdout);
+  return 0;
+}
