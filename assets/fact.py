9db9fedbbee2f1bcfa6e3797b6a3a0e90da8a7f1
Vanille-N <neven.villani@gmail.com>
2020-11-19 07:05:39 +0100

    python reproductions

---
+++ assets/fact.py
@@ -0,0 +1,31 @@
+exec(open("assets/overflow.py").read())
+
+def factorial(n):
+    f = 1
+    for i in range(1, n+1):
+        f *= i
+    return f
+
+def verify(*args):
+    if len(args) != 1:
+        return (10, "", "Usage: ./fact <n>\ncalcule et affiche la factorielle de <n>.\n")
+    else:
+        try:
+            n = int(args[0])
+        except:
+            n = 0
+        if n < 0:
+            return (10, "", "Ah non, quand meme, un nombre positif ou nul, s'il-vous-plait...\n")
+        f = sdword(factorial(n))
+        return (0, "La factorielle de {} vaut {} (en tout cas, modulo 2^32...).\n".format(n, f), "")
+
+data = [
+    [],
+    ["0"],
+    ["1"],
+    ["4"],
+    ["20"],
+    ["foo"],
+    ["-1"],
+    ["2", "1"],
+]
---
+++ assets/ordre.py
@@ -0,0 +1,8 @@
+def verify(*args):
+    return (0, "Valeur de j=7 (normalement 7), valeur de i=4.\n", "")
+
+data = [
+    [],
+    ["42"],
+    ["foo", "bar", "baz"],
+]
