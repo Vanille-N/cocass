f2c9bf4d52e6fbe62549c427285e8a67f3341d93
Vanille-N <neven.villani@gmail.com>
2020-11-25 17:28:35 +0100

    merge with new version

--- cprint.mli
+++ cprint.mli
@@ -1,5 +1,5 @@
-val print_declarations : Format.formatter -> Cparse.var_declaration list -> unit
+val print_declarations : Format.formatter -> CAST.var_declaration list -> unit
 
 val print_locator : Format.formatter -> string -> int -> int -> int -> int -> unit
 
-val print_ast : Format.formatter -> Cparse.var_declaration list -> unit
+val print_ast : Format.formatter -> CAST.var_declaration list -> unit
--- main.ml
+++ main.ml
@@ -1,28 +1,5 @@
 
-(*
- *	Copyright (c) 2005 by Laboratoire Spécification et Vérification (LSV),
- *	CNRS UMR 8643 & ENS Cachan.
- *	Written by Jean Goubault-Larrecq.  Derived from the csur project.
- *
- *	Permission is granted to anyone to use this software for any
- *	purpose on any computer system, and to redistribute it freely,
- *	subject to the following restrictions:
- *
- *	1. Neither the author nor its employer is responsible for the consequences of use of
- *		this software, no matter how awful, even if they arise
- *		from defects in it.
- *
- *	2. The origin of this software must not be misrepresented, either
- *		by explicit claim or by omission.
- *
- *	3. Altered versions must be plainly marked as such, and must not
- *		be misrepresented as being the original software.
- *
- *	4. This software is restricted to non-commercial use only.  Commercial
- *		use is subject to a specific license, obtainable from LSV.
- *)
-
-open Cparse
+open CAST
 open Verbose
 open Compile
 open Arg
@@ -53,7 +30,7 @@ let () =
 
 let () =
   let lexbuf = Lexing.from_channel (!input) in
-  let c = Ctab.translation_unit Clex.ctoken lexbuf in
+  let c = Cparse.translation_unit Clex.ctoken lexbuf in
   let out = if !c_E then stdout else open_out (!c_prefix ^ ".s") in
     Error.flush_error ();
 
