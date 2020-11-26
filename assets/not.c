616feb68f9b1e1a1ec22d983a8f15cd562e2ff17
Vanille-N <neven.villani@gmail.com>
2020-11-26 08:29:45 +0100

    some more cases

---
+++ assets/not.c
@@ -0,0 +1,6 @@
+int main() {
+    int i;
+    i = 100;
+    i = ~i;
+    return i;
+}
---
+++ failures/undeclared.c
@@ -0,0 +1,9 @@
+int main() {
+    int i;
+    j = 1;
+    {
+        int k;
+    }
+    k = 1;
+    return 0;
+}
