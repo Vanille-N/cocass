61dfdf69b0d68a3c12e5a43cbd28c0ca8cd177d2
Vanille-N <neven.villani@gmail.com>
2020-11-28 04:04:50 +0100

    generator

--- generate.ml
+++ generate.ml
@@ -72,3 +72,36 @@ let generate_idata out name =
 
 let generate_sdata out name = ()
 
+let generate_text out instr =
+    let info = if snd instr = "" then "" else "    # " ^ (snd instr) in
+    match fst instr with
+        | RET -> fprintf out "    ret%s\n\n" info
+        | QTO -> fprintf out "    cqto%s\n" info
+        | CAL fname -> fprintf out "    call %s%s\n" fname info
+        | FUN fname -> fprintf out "%s:%s\n" fname info
+        | TAG (fname, tagname) -> fprintf out "  %s.%s:%s\n" fname tagname info
+        | INC loc -> fprintf out "    incq %s%s\n" (address loc) info
+        | NOT loc -> fprintf out "    not %s%s\n" (address loc) info
+        | NEG loc -> fprintf out "    neg %s%s\n" (address loc) info
+        | DEC loc -> fprintf out "    decq %s%s\n" (address loc) info
+        | DIV loc -> fprintf out "    idiv %s%s\n" (address loc) info
+        | JMP (fname, tagname) -> fprintf out "    jmp %s.%s%s\n" fname tagname info
+        | MOV (src, dest) -> fprintf out "    mov %s, %s%s\n" (address src) (address dest) info
+        | LEA (src, dest) -> fprintf out "    lea %s, %s%s\n" (address src) (address dest) info
+        | SUB (src, dest) -> fprintf out "    sub %s, %s%s\n" (address src) (address dest) info
+        | ADD (src, dest) -> fprintf out "    add %s, %s%s\n" (address src) (address dest) info
+        | MUL (src, dest) -> fprintf out "    mul %s, %s%s\n" (address src) (address dest) info
+        | NOP -> fprintf out "%s\n" (if info = "" then "    nop" else info)
+
+let generate out prog =
+    fprintf out "    .data\n";
+    fprintf out "    .align 8\n";
+    let idata = List.rev prog.idata in
+    List.iter (generate_idata out) idata;
+    let sdata = List.rev prog.sdata in
+    List.iter (generate_sdata out) sdata;
+    fprintf out "\n";
+    fprintf out "    .global main\n";
+    fprintf out "    .text\n";
+    let text = List.rev prog.text in
+    List.iter (generate_text out) text;
