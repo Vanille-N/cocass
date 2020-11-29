d2edfcd5ec8b3957139fe03f7d6ec2dc41272d0e
Vanille-N <neven.villani@gmail.com>
2020-11-29 08:25:28 +0100

    comments

--- compile.ml
+++ compile.ml
@@ -19,11 +19,15 @@ let rec truncate n lst =
 
 let assoc x ll =
     let rec aux = function
-        | [] -> raise Not_found
+        | [] -> None
         | []::ll -> aux ll
-        | ((h,y)::l)::ll when x = h -> y
+        | ((h,y)::l)::ll when x = h -> Some y
         | (_::l)::ll -> aux (l::ll)
     in aux ll
+
+let unwrap = function
+    | Some x -> x
+    | None -> raise Not_found
 
 let verify_scope decl_list =
     let rec verify_expr declared expr = match snd expr with
@@ -62,24 +66,24 @@ let generate_asm out decl_list =
             | CIF (expr, do_true, do_false) -> failwith "TODO if"
             | CWHILE (cond, code) -> failwith "TODO while"
             | CRETURN None -> (
-                decl_asm prog (MOV (Const 0, Reg AX)) "";
-                decl_asm prog (JMP (label, "return")) "";
+                decl_asm prog (MOV (Const 0, Reg AX)) "return 0";
+                decl_asm prog (JMP (label, "return")) " +";
             )
             | CRETURN (Some ret) -> (
                 gen_expr (depth, frame, label) ret;
-                decl_asm prog (JMP (label, "return")) "";
+                decl_asm prog (JMP (label, "return")) "return";
             )
     and enter_stackframe () =
         decl_asm prog (PUSH (Reg BP)) "enter stackframe";
-        decl_asm prog (MOV (Reg SP, Reg BP)) "";
+        decl_asm prog (MOV (Reg SP, Reg BP)) " +";
     and leave_stackframe fname =
         decl_asm prog (MOV (Const 0, Reg AX)) "";
         decl_asm prog (TAG (fname, "return")) "leave stackframe";
-        decl_asm prog (POP (Reg BP)) "";
+        decl_asm prog (POP (Reg BP)) " +";
         if fname = "main" then (
-            decl_asm prog (MOV (Reg AX, Reg DI)) "return value";
-            decl_asm prog (MOV (Const 60, Reg AX)) "syscall code for exit";
-            decl_asm prog SYS "";
+            decl_asm prog (MOV (Reg AX, Reg DI)) "syscall for exit";
+            decl_asm prog (MOV (Const 60, Reg AX)) " +";
+            decl_asm prog SYS " +";
         ) else (
             decl_asm prog RET "";
         )
@@ -87,13 +91,18 @@ let generate_asm out decl_list =
         let n = List.length decs in
         let stacked = List.init (max 0 (n-6)) (fun i -> Stack (8*(i+2))) in
         let regged = truncate (min 6 n) [DI; SI; DX; CX; R8; R9] in
-        let regged = List.mapi (fun i loc ->
-            let newloc = Stack (-(i+1)*8) in
-            decl_asm prog (MOV (Reg loc, newloc)) "";
-            newloc
-        ) regged in
         let names = List.map extract_decl_name decs in
-        zip names (regged @ stacked)
+        let regged = List.mapi (fun i (loc, name) ->
+            let newloc = Stack (-(i+1)*8) in
+            decl_asm prog (MOV (Reg loc, newloc)) (sprintf "store %s" name);
+            newloc
+        ) (zip regged names) in
+        let vars = zip names (regged @ stacked) in
+        List.iter (fun (name, loc) -> match loc with
+            | Stack k when k > 0 -> decl_asm prog NOP (sprintf "%s is at rbp+%d" name k)
+            | _ -> ()
+        ) vars;
+        vars
     and make_scope depth decls =
         let n = List.length decls in
         let pos = List.init n (fun i -> Stack (-8*(i+depth))) in
@@ -101,14 +110,14 @@ let generate_asm out decl_list =
         let vars = zip names pos in
         vars
     and store depth reg =
-        decl_asm prog (MOV (Reg reg, Stack (-depth*8))) ""
+        decl_asm prog (MOV (Reg reg, Stack (-depth*8))) "store"
     and retrieve depth reg =
-        decl_asm prog (MOV (Stack (-depth*8), Reg reg)) ""
+        decl_asm prog (MOV (Stack (-depth*8), Reg reg)) "retrieve"
     and gen_decl frame = function
         | CDECL (_, name) -> ()
         | CFUN (_, name, decs, code) -> (
             label_cnt := 0;
-            decl_asm prog (FUN name) "";
+            decl_asm prog (FUN name) "toplevel function";
             let nb_args = min 6 (List.length decs) in
             enter_stackframe ();
             let args = stack_args decs in
@@ -117,38 +126,37 @@ let generate_asm out decl_list =
         )
     and gen_expr (depth, frame, label) expr = match snd expr with
         | VAR name -> (
-            let loc = assoc name frame in
+            let loc = unwrap (assoc name frame) in
             match loc with
-                | Const k -> decl_asm prog (MOV (loc, Reg AX)) (sprintf "constant value %s = %d" name k);
+                | Const k -> decl_asm prog (MOV (loc, Reg AX)) (sprintf "const val %s = %d" name k);
                 | loc -> (
-                    decl_asm prog (LEA (loc, Reg BX)) (sprintf "access %s" name);
-                    decl_asm prog (MOV (Deref BX, Reg AX)) (sprintf "read from %s" name);
+                    decl_asm prog (LEA (loc, Reg DX)) (sprintf "access %s" name);
+                    decl_asm prog (MOV (Deref DX, Reg AX)) (sprintf "read %s" name);
                 )
         )
-        | CST value -> decl_asm prog (MOV (Const value, Reg AX)) (sprintf "load value %d" value);
+        | CST value -> decl_asm prog (MOV (Const value, Reg AX)) (sprintf "load val %d" value);
         | STRING str -> (
-            let name = sprintf "__str_%d" !str_count in
+            let name = sprintf ".LC%d" !str_count in
             incr str_count;
             decl_str prog name str;
-            decl_asm prog (LEA (Glob name, Reg BX)) (sprintf "access %s" name);
-            decl_asm prog (MOV (Reg BX, Reg AX)) (sprintf "read from %s" name);
+            decl_asm prog (LEA (Glob name, Reg DX)) (sprintf "access %s" name);
+            decl_asm prog (MOV (Reg DX, Reg AX)) (sprintf "read %s" name);
         )
         | SET_VAR (name, expr) -> (
             gen_expr (depth, frame, label) expr;
-            let loc = assoc name frame in
-            decl_asm prog (LEA (loc, Reg BX)) (sprintf "access %s" name);
-            decl_asm prog (MOV (Reg AX, Deref BX)) (sprintf "write to %s" name);
+            let loc = unwrap (assoc name frame) in
+            decl_asm prog (LEA (loc, Reg DX)) (sprintf "access %s" name);
+            decl_asm prog (MOV (Reg AX, Deref DX)) (sprintf "write %s" name);
         )
         | SET_ARRAY (arr, idx, value) -> (
-            let loc = assoc arr frame in
+            let loc = unwrap (assoc arr frame) in
             gen_expr (depth, frame, label) idx;
             store depth AX;
             gen_expr (depth+1, frame, label) value;
             retrieve (depth) CX;
-            decl_asm prog NOP "now load array base";
-            decl_asm prog (MOV (loc, Reg DX)) "";
-            decl_asm prog (LEA (Index (DX, CX, 8), Reg BX)) "";
-            decl_asm prog (MOV (Reg AX, Deref BX)) "";
+            decl_asm prog (MOV (loc, Reg DX)) "access array";
+            decl_asm prog (LEA (Index (DX, CX), Reg DX)) " +";
+            decl_asm prog (MOV (Reg AX, Deref DX)) " +";
         )
         | CALL (fname, expr_lst) -> (
             List.iteri (fun i e ->
@@ -160,38 +168,38 @@ let generate_asm out decl_list =
             let nb_stacked = max 0 (nb_args-6) in
             let reg_dests = truncate nb_regged [DI; SI; DX; CX; R8; R9] in
             let reg_dests = List.map (fun r -> Reg r) reg_dests in
-            let stack_dests = List.init nb_stacked (fun i -> Stack (-(depth+nb_args+i)*8)) in
+            let stack_dests = List.init nb_stacked (fun i -> Stack (-(depth+nb_args+nb_stacked-i)*8)) in
             let dests = reg_dests @ stack_dests in
             let locs = List.init nb_args (fun i -> Stack (-(depth+i)*8)) in
             let moves = zip locs dests in
-            List.iter (fun (loc, dest) ->
+            List.iteri (fun i (loc, dest) ->
                 match dest with
                     | Stack k -> (
-                        decl_asm prog (MOV (loc, Reg AX)) "";
-                        decl_asm prog (MOV (Reg AX, dest)) "";
+                        decl_asm prog (MOV (loc, Reg AX)) (sprintf "%d'th arg" i);
+                        decl_asm prog (MOV (Reg AX, dest)) " +";
                     )
-                    | Reg r -> decl_asm prog (MOV (loc, dest)) ""
+                    | Reg r -> decl_asm prog (MOV (loc, dest)) (sprintf "%d'th arg" i)
                     | _ -> failwith "unreachable @ compile::generate_asm::gen_expr::CALL::iter::_"
             ) moves;
-            decl_asm prog (SUB (Const ((depth+nb_args)*8), Reg SP)) "";
-            decl_asm prog (MOV (Const nb_stacked, Reg AX)) "varargs";
-            decl_asm prog (CALL fname) "";
-            decl_asm prog (ADD (Const ((depth+nb_args)*8), Reg SP)) "";
+            decl_asm prog (SUB (Const ((depth+nb_args+nb_stacked)*8), Reg SP)) (sprintf "%d locals" (depth+nb_args));
+            decl_asm prog (MOV (Const nb_stacked, Reg AX)) (sprintf "varargs: %d on the stack" nb_stacked);
+            decl_asm prog (CALL fname) " +";
+            decl_asm prog (ADD (Const ((depth+nb_args+nb_stacked)*8), Reg SP)) " +";
         )
         | OP1 (op, expr) -> (
             gen_expr (depth, frame, label) expr;
             match op with
-                | M_MINUS -> decl_asm prog (NEG (Reg AX)) "";
-                | M_NOT -> decl_asm prog (NOT (Reg AX)) "";
-                | M_POST_INC -> decl_asm prog (INC (Deref BX)) "";
-                | M_POST_DEC -> decl_asm prog (DEC (Deref BX)) "";
+                | M_MINUS -> decl_asm prog (NEG (Reg AX)) "negative";
+                | M_NOT -> decl_asm prog (NOT (Reg AX)) "bitwise not";
+                | M_POST_INC -> decl_asm prog (INC (Deref DX)) "incr (post)";
+                | M_POST_DEC -> decl_asm prog (DEC (Deref DX)) "decr (post)";
                 | M_PRE_INC -> (
-                    decl_asm prog (INC (Deref BX)) "";
-                    decl_asm prog (INC (Reg AX)) "";
+                    decl_asm prog (INC (Deref DX)) "incr (pre)";
+                    decl_asm prog (INC (Reg AX)) " +";
                 )
                 | M_PRE_DEC -> (
-                    decl_asm prog (DEC (Deref BX)) "";
-                    decl_asm prog (DEC (Reg AX)) "";
+                    decl_asm prog (DEC (Deref DX)) "decr (pre)";
+                    decl_asm prog (DEC (Reg AX)) " +";
                 )
         )
         | OP2 (op, lhs, rhs) -> (
@@ -201,7 +209,7 @@ let generate_asm out decl_list =
                     store depth AX;
                     gen_expr (depth+1, frame, label) rhs;
                     retrieve depth CX;
-                    decl_asm prog (MUL (Reg CX)) "";
+                    decl_asm prog (MUL (Reg CX)) "mul";
                 )
                 | S_MOD -> (
                     gen_expr (depth, frame, label) lhs;
@@ -210,8 +218,8 @@ let generate_asm out decl_list =
                     decl_asm prog (MOV (Reg AX, Reg CX)) "";
                     retrieve depth AX;
                     decl_asm prog CQTO "";
-                    decl_asm prog (DIV (Reg CX)) "";
-                    decl_asm prog (MOV (Reg DX, Reg AX)) "";
+                    decl_asm prog (DIV (Reg CX)) "div/mod";
+                    decl_asm prog (MOV (Reg DX, Reg AX)) " + -> mod";
                 )
                 | S_DIV -> (
                     gen_expr (depth, frame, label) lhs;
@@ -220,30 +228,30 @@ let generate_asm out decl_list =
                     decl_asm prog (MOV (Reg AX, Reg CX)) "";
                     retrieve depth AX;
                     decl_asm prog CQTO "";
-                    decl_asm prog (DIV (Reg CX)) "";
+                    decl_asm prog (DIV (Reg CX)) "div/mod -> div";
                 )
                 | S_ADD -> (
                     gen_expr (depth, frame, label) lhs;
                     store depth AX;
                     gen_expr (depth+1, frame, label) rhs;
                     retrieve depth CX;
-                    decl_asm prog (ADD (Reg CX, Reg AX)) "";
+                    decl_asm prog (ADD (Reg CX, Reg AX)) "add";
                 )
                 | S_SUB -> (
                     gen_expr (depth, frame, label) lhs;
                     store depth AX;
                     gen_expr (depth+1, frame, label) rhs;
                     retrieve depth CX;
-                    decl_asm prog (NEG (Reg AX)) "";
-                    decl_asm prog (ADD (Reg CX, Reg AX)) "";
+                    decl_asm prog (NEG (Reg AX)) "sub: neg";
+                    decl_asm prog (ADD (Reg CX, Reg AX)) "    & add";
                 )
                 | S_INDEX -> (
                     gen_expr (depth, frame, label) lhs;
                     store depth AX;
                     gen_expr (depth+1, frame, label) rhs;
                     retrieve depth CX;
-                    decl_asm prog (LEA (Index (CX, AX, 8), Reg BX)) "";
-                    decl_asm prog (MOV (Deref BX, Reg AX)) "";
+                    decl_asm prog (LEA (Index (CX, AX), Reg DX)) "";
+                    decl_asm prog (MOV (Deref DX, Reg AX)) "";
                 )
         )
         | CMP (op, lhs, rhs) -> (
@@ -251,33 +259,33 @@ let generate_asm out decl_list =
             store depth AX;
             gen_expr (depth+1, frame, label) rhs;
             retrieve depth CX;
-            decl_asm prog (CMP (Reg AX, Reg CX)) "";
+            decl_asm prog (CMP (Reg AX, Reg CX)) "compare";
             let tagbase = sprintf "%d_cmp_" !label_cnt in
             incr label_cnt;
             match op with
                 | C_LT -> (
-                    decl_asm prog (JLT (label, tagbase ^ "lt")) "";
+                    decl_asm prog (JLT (label, tagbase ^ "lt")) "case <";
                     decl_asm prog (MOV (Const 0, Reg AX)) "";
-                    decl_asm prog (JMP (label, tagbase ^ "done")) "";
-                    decl_asm prog (TAG (label, tagbase ^ "lt")) "";
-                    decl_asm prog (MOV (Const 1, Reg AX)) "";
-                    decl_asm prog (TAG (label, tagbase ^ "done")) "";
+                    decl_asm prog (JMP (label, tagbase ^ "done")) " +";
+                    decl_asm prog (TAG (label, tagbase ^ "lt")) " +";
+                    decl_asm prog (MOV (Const 1, Reg AX)) " +";
+                    decl_asm prog (TAG (label, tagbase ^ "done")) " +";
                 )
                 | C_LE -> (
-                    decl_asm prog (JLE (label, tagbase ^ "le")) "";
-                    decl_asm prog (MOV (Const 0, Reg AX)) "";
-                    decl_asm prog (JMP (label, tagbase ^ "done")) "";
-                    decl_asm prog (TAG (label, tagbase ^ "le")) "";
-                    decl_asm prog (MOV (Const 1, Reg AX)) "";
-                    decl_asm prog (TAG (label, tagbase ^ "done")) "";
+                    decl_asm prog (JLE (label, tagbase ^ "le")) "case <=";
+                    decl_asm prog (MOV (Const 0, Reg AX)) " +";
+                    decl_asm prog (JMP (label, tagbase ^ "done")) " +";
+                    decl_asm prog (TAG (label, tagbase ^ "le")) " +";
+                    decl_asm prog (MOV (Const 1, Reg AX)) " +";
+                    decl_asm prog (TAG (label, tagbase ^ "done")) " +";
                 )
                 | C_EQ -> (
-                    decl_asm prog (JEQ (label, tagbase ^ "eq")) "";
-                    decl_asm prog (MOV (Const 0, Reg AX)) "";
-                    decl_asm prog (JMP (label, tagbase ^ "done")) "";
-                    decl_asm prog (TAG (label, tagbase ^ "eq")) "";
-                    decl_asm prog (MOV (Const 1, Reg AX)) "";
-                    decl_asm prog (TAG (label, tagbase ^ "done")) "";
+                    decl_asm prog (JEQ (label, tagbase ^ "eq")) "case ==";
+                    decl_asm prog (MOV (Const 0, Reg AX)) " +";
+                    decl_asm prog (JMP (label, tagbase ^ "done")) " +";
+                    decl_asm prog (TAG (label, tagbase ^ "eq")) " +";
+                    decl_asm prog (MOV (Const 1, Reg AX)) " +";
+                    decl_asm prog (TAG (label, tagbase ^ "done")) " +";
                 )
         )
         | EIF (cond, expr_true, expr_false) -> failwith "TODO eif"
