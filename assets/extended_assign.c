93911a401be8d797542c932b797b4c4533003a13
Vanille-N <neven.villani@gmail.com>
2020-12-01 09:16:05 +0100

    extended assignment test

--- assets/extended_assign.c
+++ assets/extended_assign.c
@@ -7,13 +7,13 @@ int main() {
     j += 1;
     printf("%d ?= 2\n", j);
     i[0] += 1;
-    printf("%d ?= 5\n", i[0]);
+    printf("%d ?= 6\n", i[0]);
     *i += 1;
-    printf("%d ?= 6\n", *i);
-    j *= 2;
-    printf("%d ?= 4\n", j);
-    i[0] *= 2;
-    printf("%d ?= 12\n", i[0]);
-    *i *= 2;
-    printf("%d ?= 24\n", *i);
+    printf("%d ?= 7\n", *i);
+    // j *= 2;
+    // printf("%d ?= 4\n", j);
+    // i[0] *= 2;
+    // printf("%d ?= 12\n", i[0]);
+    // *i *= 2;
+    // printf("%d ?= 24\n", *i);
 }
