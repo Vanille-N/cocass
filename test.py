07c6a21f7f53c3cd1d157ed57c73a1d4db876e42
Vanille-N <neven.villani@gmail.com>
2020-12-04 07:54:27 +0100

    improve auto checker

--- test.py
+++ test.py
@@ -4,8 +4,27 @@ from subprocess import run
 import sys
 
 assets = [
-    "ex0", "ex1", "ex2", "add", "ret",  # trivial
-    "argcount", "noreturn", "multicall", # arguments and function calls
+    ("easy",
+        "ex0", "ex1", "ex2", "add",
+        "ret", "scoped_ret",
+    ),
+    ("call",
+        "argcount", "noreturn", "multicall",
+        "has_args", "hello", "argsort",
+        "call", "exit", "put", "printglob",
+    ),
+    ("calc",
+        "fact", "binops", "bitwise", "cmp",
+        "ordre", "incdec", "cmp_order", "shifts",
+    ),
+    ("ptr",
+    ),
+    ("flow",
+        "break", "continue", "count",
+    ),
+    ("reduce",
+        "reduce_eif",
+    )
 ]
 
 class Module:
@@ -17,8 +36,8 @@ class Module:
             instr = "self.{elem} = d['{elem}']".format(elem=k)
             exec(instr)
 
-def compile(cc, fbase, more=[]):
-    res = run([cc, "assets/{}.c".format(fbase), *more], capture_output=True)
+def compile(cc, fbase):
+    res = run([cc, "assets/{}.c".format(fbase)], capture_output=True)
     success = True
     if res.returncode != 0:
         print("Errored: retcode {}".format(res.returncode))
@@ -30,52 +49,96 @@ def compile(cc, fbase, more=[]):
         success = False
     return success
 
+def compare(lt, rt):
+    if lt == rt:
+        return None
+    def context(s, pos, l):
+        return s[max(0, pos-l) : min(len(s), pos+l)]
+    for i in range(len(lt)):
+        if lt[i] != rt[i]:
+            return (i, context(lt, i, 10), context(rt, i, 10))
+
 def check(fbase):
     module = Module("verify/{}.py".format(fbase))
     for d in module.data:
-        res = run(["./assets/{}".format(fbase), *d], capture_output=True)
+        prog = "./assets/{}".format(fbase)
+        res = run([prog, *d], capture_output=True)
         code, out, err = res.returncode, res.stdout, res.stderr
-        (expect_code, expect_out, expect_err) = module.verify(*d)
+        (expect_code, expect_out, expect_err) = module.verify(prog, *d)
         expect_out, expect_err = bytes(expect_out, 'UTF-8'), bytes(expect_err, 'UTF-8')
         if expect_code == code and expect_err == err and expect_out == out:
             print("    \x1b[32m[OK]\x1b[0m {}".format(d))
         else:
             print("    \x1b[31m[KO]\x1b[0m {}".format(d))
             if expect_code != code:
-                print("        \x1b[33mWrong code: \x1b[32m[{}] \x1b[31m[{}]\x1b[0m".format(expect_code, code))
-            if expect_out != out:
-                print("        \x1b[33mWrong stdout: \x1b[32m[{}] \x1b[31m[{}]\x1b[0m".format(expect_out, out))
-            if expect_err != err:
-                print("        \x1b[33mWrong stderr: \x1b[32m[{}] \x1b[31m[{}]\x1b[0m".format(expect_err, err))
+                print("        \x1b[33mWrong code:")
+                print("              \x1b[32m[{}]".format(expect_code))
+                print("              \x1b[31m[{}]\x1b[0m".format(code))
+            diff = compare(expect_out, out)
+            if diff is not None:
+                print("        \x1b[33mWrong stdout:")
+                print("              \x1b[32m[{}]".format(expect_out))
+                print("              \x1b[31m[{}]\x1b[0m".format(out))
+                print("        Difference at {}: [{}] vs [{}]".format(*diff))
+            diff = compare(expect_err, err)
+            if diff is not None:
+                print("        \x1b[33mWrong stderr:")
+                print("              \x1b[32m[{}]".format(expect_err))
+                print("              \x1b[31m[{}]\x1b[0m".format(err))
+                print("        Difference at {}: [{}] vs [{}]".format(*diff))
 
 
-def fulltest(cc, fbase, more=[]):
+def fulltest(cc, fbase):
     print("Running {}:".format(fbase))
-    if compile(cc, fbase, more):
+    if compile(cc, fbase):
         check(fbase)
         return True
     return False
 
 def main():
-    if len(sys.argv) >= 2:
-        cc = sys.argv[1]
-        if cc == "gcc":
-            more = lambda fbase: ["-o", "assets/{}".format(fbase)]
-        elif cc == "clang":
-            more = lambda fbase: ["-o", "assets/{}".format(fbase)]
-        elif cc == "mcc":
-            cc = "./mcc"
-            more = lambda x: []
-        else:
-            more = lambda x: []
+    if len(sys.argv) == 2:
+        if sys.argv[1] == "help":
+            print("Automated tester for a C-- compiler")
+            print("  usage:")
+            print("      test file [file2, ...]")
+            print("              check the compiled output by mcc")
+            print("              of assets/file.c against the specification in verify/file.py")
+            print("      test --category")
+            print("              execute check for all tests from the given category")
+            print("      test")
+            print("              execute all checks")
+            print("      test help")
+            print("              print this help")
+            print("      test categ")
+            print("              print all categories")
+            print("      test list")
+            print("              print all files")
+            return
+        elif sys.argv[1] == "categ":
+            for (category, *tests) in assets:
+                print(category)
+            return
+        elif sys.argv[1] == "list":
+            for (category, *tests) in assets:
+                print("{}: {}".format(category, ", ".join(tests)))
+            return
+
+    cc = "./mcc"
+    args = sys.argv[1:]
+
+    if len(args) >= 1:
+        for fbase in args:
+            if len(fbase) >= 2 and fbase[0:2] == "--":
+                for (category, *tests) in assets:
+                    if category == fbase[2:]:
+                        for fbase in tests:
+                            fulltest(cc, fbase)
+            else:
+                fulltest(cc, fbase)
     else:
-        cc = "./mcc"
-        more = lambda x: []
-    if len(sys.argv) >= 3:
-        bases = sys.argv[2:]
-    else:
-        bases = assets
-    for fbase in bases:
-        fulltest(cc, fbase, more=more(fbase))
+        for (category, *tests) in assets:
+            print("\n\n    <<< category: {} >>>\n".format(category))
+            for fbase in tests:
+                fulltest(cc, fbase)
 
 main()
