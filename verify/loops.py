3381ec92b52d317879442086af75da92883e7919
Vanille-N <neven.villani@gmail.com>
2020-12-06 08:22:04 +0100

    expected output for loops

---
+++ verify/loops.py
@@ -0,0 +1,16 @@
+def verify(*args):
+    res = ""
+    res += "1. for                   0 1 2 3 4 5 6 7 8 9 \n"
+    res += "2. one-line for          0 1 2 3 4 5 6 7 8 9 \n"
+    res += "3. for no-inc            0 1 2 3 4 5 6 7 8 9 \n"
+    res += "4. for no-init           0 1 2 3 4 5 6 7 8 9 \n"
+    res += "5. for only-cond         0 1 2 3 4 5 6 7 8 9 \n"
+    res += "6. while                   1 2 3 4 5 6 7 8 9 \n"
+    res += "7. do-while              0 1 2 3 4 5 6 7 8 9 \n"
+    res += "8. one-line while          1 2 3 4 5 6 7 8 9 \n"
+    res += "9. one-line do-while     0 1 2 3 4 5 6 7 8 9 "
+    return (0, res, "")
+
+data = [
+    [],
+]
