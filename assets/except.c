8c85ff5f88c807264fcba57f65788b0de6a705d1
Vanille-N <neven.villani@gmail.com>
2020-12-07 07:04:43 +0100

    verifier for except

--- assets/except.c
+++ assets/except.c
@@ -7,42 +7,46 @@ int test_throw(int i) {
     return 42;
 }
 
-// int test_catch(int i) {
-//     int j;
-//     try {
-//         throw Bar(7);
-//         // j = test_throw(i);
-//         // j = 10;
-//     } catch (Foo x) {
-//         printf("Failed n.1 with Foo(%d)\n", x);
-//     }
-//     return j;
-// }
-//
-// int test_catch_finally (int i) {
-//     try {
-//         return test_throw(i);
-//     } catch (Bar x) {
-//         printf("Failed n.2 with Bar(%d)\n", x);
-//         // if (x == 1) { throw Bar(5); }
-//     } finally {
-//         printf("Ended n.2\n");
-//         // throw Baz(5);
-//     }
-// }
-//
-// int test_finally (int i) {
-//     try {
-//         return test_throw(i);
-//     } finally {
-//         printf("Ended n.2\n");
-//     }
-// }
-//
-int test_multi_catch (int i) {
-    int i;
+int test_catch(int i) {
+    int j;
+    j = -1;
     try {
-        i = test_throw(i);
+        j = test_throw(i);
+    } catch (Foo x) {
+        printf("Failed n.1 with Foo(%d)\n", x);
+    }
+    return j;
+}
+
+int test_catch_finally (int i) {
+    int j;
+    j = -1;
+    try {
+        j = test_throw(i);
+    } catch (Bar x) {
+        printf("Failed n.2 with Bar(%d)\n", x);
+    } finally {
+        printf("Ended n.2\n");
+    }
+    return j;
+}
+
+int test_finally (int i) {
+    int j;
+    j = -1;
+    try {
+        j = test_throw(i);
+    } finally {
+        printf("Ended n.2\n");
+    }
+    return j;
+}
+
+int test_multi_catch (int i) {
+    int j;
+    j = -1;
+    try {
+        j = test_throw(i);
     } catch (Foo x) {
         printf("Caught Foo(%d)\n", x);
     } catch (Bar y) {
@@ -53,19 +57,19 @@ int test_multi_catch (int i) {
     } finally {
         printf("Finally...\n");
     }
-    return i;
+    return j;
 }
-//
-// int test_no_error (int i) {
-//     try {
-//         printf("Everything is fine.\n");
-//     } catch (Foo x) {
-//         printf("Unreachable.\n");
-//     } finally {
-//         printf("No error occurred.\n");
-//     }
-//     return 10;
-// }
+
+int test_no_error (int i) {
+    try {
+        printf("Everything is fine.\n");
+    } catch (Foo x) {
+        printf("Unreachable.\n");
+    } finally {
+        printf("No error occurred.\n");
+    }
+    return 10;
+}
 
 int test_string () {
     try {
@@ -75,32 +79,31 @@ int test_string () {
             printf("Caught %s\n", x);
             throw Foo(x);
         }
-        // throw Baz("Nope");
     } catch (Foo x) {
         printf("Caught %s again\n", x);
-        throw Foo(x);
     }
 }
 
-int main() {
-    // throw Foo(15);
-    // test_throw(0);
-    // test_throw(1);
-    // test_throw(2);
-    // OK
-    // test_catch(0);
-    // test_catch(1);
-    // test_catch(2);
-    // test_catch_finally(1);
-    // test_catch_finally(1);
-    // test_catch_finally(2);
-    // test_finally(0);
-    // test_finally(1);
-    // test_finally(2);
-    // test_multi_catch(0);
-    test_string();
-    // test_multi_catch(1);
-    // test_multi_catch(2);
-    // test_no_error(0);
-    printf("Normal exit");
+int main(int argc, char** argv) {
+    switch (atoi(argv[1])) {
+        case 1: throw Foo(15); break;
+        case 2: test_throw(0); break;
+        case 3: test_throw(1); break;
+        case 4: test_throw(2); break;
+        case 5: test_catch(0); break;
+        case 6: test_catch(1); break;
+        case 7: test_catch(2); break;
+        case 8: test_catch_finally(0); break;
+        case 9: test_catch_finally(1); break;
+        case 10: test_catch_finally(2); break;
+        case 11: test_finally(0); break;
+        case 12: test_finally(1); break;
+        case 13: test_finally(2); break;
+        case 14: test_multi_catch(0); break;
+        case 15: test_string(); break;
+        case 16: test_multi_catch(1); break;
+        case 17: test_multi_catch(2); break;
+        case 18: test_no_error(0); break;
+    }
+    printf("Normal exit\n");
 }
