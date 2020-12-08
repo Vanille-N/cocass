0ac324a12766aa82ad88781b65541431ff6242ae
Vanille-N <neven.villani@gmail.com>
2020-12-08 11:38:00 +0100

    test for arity warnings

---
+++ failures/arity.c
@@ -0,0 +1,20 @@
+int foo() {}
+int bar(int a) {}
+int baz(int a, int b) {}
+
+int main() {
+    foo();
+    foo(1);
+    foo(1, 1);
+    bar();
+    bar(1);
+    bar(1, 2);
+    baz();
+    baz(1);
+    baz(1, 2);
+    malloc();
+    malloc(0, 1, 2);
+    printf();
+    fprintf(stdout);
+    quux();
+}
