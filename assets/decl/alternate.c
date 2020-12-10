66299c19197c8f58d5e0225bc539214ac0cd13d7
Vanille-N <neven.villani@gmail.com>
2020-12-10 07:59:31 +0100

    tests for improved parser

---
+++ assets/decl/alternate.c
@@ -0,0 +1,28 @@
+int main () {
+    int i, ibis;
+    i = 1;
+    int j;
+    int jbis;
+    j = 2;
+    jbis = 3;
+    jbis = 4;
+    int k;
+    k = i + j;
+    printf("%d %d %d\n", i, j, k);
+}
+
+
+int foo() {
+    int i;
+}
+
+int bar() {
+    return 0;
+}
+
+int baz() {
+    return 0;
+    int i;
+    int j;
+    return 1;
+}
---
+++ failures/decl_alternate.c
@@ -0,0 +1,5 @@
+int main() {
+    int i;
+    j = 1;
+    int j;
+}
