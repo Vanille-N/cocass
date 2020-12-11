f28d9b81755421fb6d9943643f2cac1b717e5452
Vanille-N <neven.villani@gmail.com>
2020-12-11 11:17:23 +0100

    thinking about it, this is actually a better solution

--- assets/reduce/reduce_monops.c
+++ assets/reduce/reduce_monops.c
@@ -1,11 +1,7 @@
-int verify(int x) {
-    if (!x) exit(10);
-}
-
 int main() {
     int x;
     x = ~10;
-    verify(x == ~10);
+    assert(x == ~10);
     x = -12;
-    verify(x == -12);
+    assert(x == -12);
 }
