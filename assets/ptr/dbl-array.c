9602ea19ac521846dee0adff10ae8f0b00ac2f8d
Vanille-N <neven.villani@gmail.com>
2020-12-24 09:54:46 +0100

    check prints

--- assets/ptr/dbl-array.c
+++ assets/ptr/dbl-array.c
@@ -7,4 +7,8 @@ int main() {
         k[1] = 2*i+2;
         arr[i] = k;
     }
+    arr[0][0]++;
+    printf("[ %d %d ]\n", arr[0][0], arr[0][1]);
+    printf("[ %d %d ]\n", arr[1][0], arr[1][1]);
+    printf("[ %d %d ]\n", arr[2][0], arr[2][1]);
 }
