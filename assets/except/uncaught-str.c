1dc7bce232e3faa5671935a98dba36436f59577d
Vanille-N <neven.villani@gmail.com>
2020-12-24 10:18:19 +0100

    revamped test for uncaught string exception

--- assets/except/uncaught-str.c
+++ assets/except/uncaught-str.c
@@ -1,3 +1,10 @@
-int main() {
-    throw Exc("Hello, World");
+char* s = "foo";
+
+int main(int argc, char** argv) {
+    char* z = "bar";
+    switch (atol(argv[1])) {
+        case 0: throw Zero("Hello, World");
+        case 1: throw One(s);
+        case 2: throw Two(z);
+    }
 }
