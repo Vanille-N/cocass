d911b7cc589740ba5fbb38f8ffc0c64d1fbd2d19
Vanille-N <neven.villani@gmail.com>
2020-12-24 10:19:41 +0100

    update verifier

--- verify/except/uncaught-str.py
+++ verify/except/uncaught-str.py
@@ -1,6 +1,18 @@
 def expect(*args):
-    return (111, "", "Unhandled exception Exc(\"Hello, World\")\n")
+    n = int(args[1])
+    if n == 0:
+        return (111, "", "Unhandled exception Zero(\"Hello, World\")\n")
+    elif n == 1:
+        return (111, "", "Unhandled exception One(\"foo\")\n")
+    elif n == 2:
+        return (111, "", "Unhandled exception Two(\"bar\")\n")
+    else:
+        return (0, "", "")
+
 
 data = [
-    [],
+    ["-1"],
+    ["0"],
+    ["1"],
+    ["2"],
 ]
