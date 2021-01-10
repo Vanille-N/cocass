1ac014ddb4391524106bd26baa1310e462c7a68b
Vanille-N <neven.villani@gmail.com>
2021-01-10 10:00:53 +0100

    adapt quine tester to reformat

--- verify/string/quine.py
+++ verify/string/quine.py
@@ -1,7 +1,6 @@
 q = """int main() {
-\tchar* c;
-\tc = "int main() {%c%cchar* c;%c%cc = %c%s%c;%c%cprintf(c,10,9,10,9,34,c,34,10,9,10,9,10,10);%c%creturn 0;%c}%c";
-\tprintf(c,10,9,10,9,34,c,34,10,9,10,9,10,10);
+\tchar* c = "int main() {%c%cchar* c = %c%s%c;%c%cprintf(c,10,9,34,c,34,10,9,10,9,10,10);%c%creturn 0;%c}%c";
+\tprintf(c,10,9,34,c,34,10,9,10,9,10,10);
 \treturn 0;
 }
 """
