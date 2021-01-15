6db6570494d6e77f911d39b42bff3248d740b8f4
Vanille-N <neven.villani@gmail.com>
2021-01-15 10:03:18 +0100

    binconv use exception

--- tests/string/binconv.c
+++ tests/string/binconv.c
@@ -8,7 +8,7 @@ int bin_to_int (char* t, int len) {
         switch (*(t+i) & BYTE) {
             case '1': r += p; break;
             case '0': break;
-            default: return 0;
+            default: throw InvalidBinary(*(t+i) & BYTE);
         }
         p *= 2;
     }
@@ -19,6 +19,10 @@ int main () {
     DECLARE(char* a = "101");
     DISPLAY(bin_to_int(a, 3));
     DISPLAY(bin_to_int(a, 2));
-    DISPLAY(bin_to_int("110f2o", 6));
+    try {
+        DISPLAY(bin_to_int("110f2o", 6));
+    } catch (InvalidBinary c) {
+        printf("Invalid character '%c' in \"110f2o\"\n", c);
+    }
     DISPLAY(bin_to_int("110f2o", 3));
 }
--- verify/string/binconv.py
+++ verify/string/binconv.py
@@ -3,7 +3,7 @@ def expect(*args):
     res += 'char* a = "101"\n'
     res += 'bin_to_int(a, 3) = 5\n'
     res += 'bin_to_int(a, 2) = 1\n'
-    res += 'bin_to_int("110f2o", 6) = 0\n'
+    res += 'Invalid character \'f\' in "110f2o"\n'
     res += 'bin_to_int("110f2o", 3) = 3\n'
     return (0, res, "")
 
